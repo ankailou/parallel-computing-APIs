d1a6070693fb60c4f27ff57129ab13111364f1fe
Ankai Lou <ankailou@gmail.com>
2015-11-04 12:17:55 -0500

    lab 1 b - persistent threads

---
+++ cse5441_lab1b/convergence.cc
@@ -0,0 +1,108 @@
+#include "lab1.h"
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <pthread.h>
+using namespace std;
+
+/*****************************************************************************
+ *********** helper functions for computing the convergence loops ************
+ *****************************************************************************/
+
+/**
+ * function: convergenceLoop
+ ***************************
+ * loop 1: compute new dsv score for each box; loop 2: communicate dsv scores;
+ *
+ * param numThreads: number of threads to create
+ * updates: for each @box in @Boxes: @box.dsv = computeNewDSV(@box);
+ */
+void convergenceLoop(int numThreads) {
+    int rc, index;
+    //pthread_t *thread;
+    //thread = (pthread_t *)malloc(sizeof(*thread)*numThreads);
+    for (long i = 0; i < numBoxes; i++) {
+        index = i % numThreads;
+        rc = pthread_create(&thread[index], NULL, computeNewDSV, (void *)i);
+        if (index == numThreads - 1 || index == (numBoxes - 1)%numThreads) {
+            for (long k = 0; k <= index; k++)
+                rc = pthread_join(thread[k], NULL);
+        }
+    }
+    for (int j = 0; j < numBoxes; j++)
+        Boxes[j].dsv = Boxes[j].dsvNew;
+}
+
+/*****************************************************************************
+ *************** helper function for computing new dsv values ****************
+ *****************************************************************************/
+
+/**
+ * function: computeNewDSV
+ *************************
+ * for each side, average adjacent temperature = sum(intersection * dsv) / perimeter;
+ *
+ * param var: void pointer representing long representing @box id value;
+ * updates: @box.newDSV = @box.dsv - [AFFECT_RATE * (@box.dsv - avg_adjacent_temp)];
+ */
+void *computeNewDSV(void *var) {
+    long index = (long)var;
+    Box box = Boxes[index];
+    int perimeter = 0;
+    float avgAdjacent = 0.0, offset = 0.0;
+    int numNeighbors[NUM_SIDES] = {box.nTop, box.nLeft, box.nBottom, box.nRight};
+    for (int i = 0; i < NUM_SIDES; i++) {
+        if (numNeighbors[i] > 0)
+            perimeter += (i % 2 == 0) ? (box).w : (box).h;
+        avgAdjacent += computeSide(&(box), i, numNeighbors[i]);
+    }
+    if (perimeter > 0)
+        offset = AFFECT_RATE * (box.dsv - (avgAdjacent / (float)perimeter));
+    Boxes[index].dsvNew = box.dsv - offset;
+    pthread_exit(NULL);
+}
+
+/**
+ * function: computeSide
+ ***********************
+ * compute average adjacent temperature for one side of @box;
+ *
+ * param box: struct holding box information;
+ * param side: int representing side := { 0 : top, 1 : left, 2 : bottom, 3 : right };
+ * param numNeighbors: int representing number of neighboring boxes to iterate;
+ * returns: sum_{nbox in @Boxes}( intersect(nBox,@box) * nBox.dsv );
+ */
+float computeSide(Box *box, int side, int numNeighbors) {
+    int is;
+    float temp = 0.0;
+    int *sides[4] = {(*box).topNeighbor, (*box).leftNeighbor, (*box).bottomNeighbor, (*box).rightNeighbor};
+    for (int i = 0; i < numNeighbors; i++) {
+        Box neighbor = Boxes[sides[side][i]];
+        if (side % 2 == 0)
+            is = intersect((*box).x, (*box).w, neighbor.x, neighbor.w);
+        else
+            is = intersect((*box).y, (*box).h, neighbor.y, neighbor.h);
+        temp += neighbor.dsv * is;
+    }
+    return temp;
+}
+
+/**
+ * function: intersect
+ *********************
+ * compute the length of the intersection between adjacent boxes;
+ *
+ * param s1: start index of side of box 1;
+ * param l1: length of the side of box 1;
+ * param s2: start index of side of box 2;
+ * param l2: length of the side of box 2;
+ * returns: l1 - [ segments of s2 + l2 not intersecting with box 1 ];
+ */
+int intersect(int s1, int l1, int s2, int l2) {
+    int intersect = l1;
+    if ( s2 > s1 )
+        intersect -= (s2 - s1);
+    if ((s2 + l2) < (s1 + l1))
+        intersect -= ((s1 + l1) - (s2 + l2));
+    return intersect;
+}
---
+++ cse5441_lab1b/lab1.cc
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cstdlib>
+#include <sstream>
+#include <sys/time.h>
+#include <pthread.h>
+#include "lab1.h"
+using namespace std;
+
+/*****************************************************************************
+ ***************************** global veriables ******************************
+ *****************************************************************************/
+
+int numBoxes, numRows, numCols, loops = 0;
+pthread_t *thread;
+float runtime = 0.0;
+map<int,Box> Boxes;
+
+/*****************************************************************************
+ ******************************* main function *******************************
+ *****************************************************************************/
+
+/**
+ * function: main
+ ****************
+ * populate boxes; loop until convergence condition; otherwise compute dsv
+ *
+ * param argv[1]: name of file to read box parameters
+ * param argv[2]: number of threads to use
+ */
+int main(int argc, char* argv[]) {
+    int numThreads;
+    struct timeval t1, t2;
+    populateBoxes(argv[1]);
+    istringstream ss(argv[2]);
+    ss >> numThreads;
+    // create persistent threads
+    thread = (pthread_t *)malloc(sizeof(*thread)*numThreads);
+    while ( !convergenceCondition() ) {
+        loops++;
+        gettimeofday(&t1, NULL);
+        convergenceLoop(numThreads);
+        gettimeofday(&t2, NULL);
+        float diff = ((unsigned long long)t2.tv_usec - (unsigned long long)t1.tv_usec) / 1000000.0;
+        //cout << "Loop " << loops << ": " << diff << " seconds!" << endl;
+        runtime += diff;
+    }
+
+    cout << "Convergence Loops Complete. Freeing Memory..." << endl;
+    freeBoxes();
+    cout << "Number of Convergence Loops: " << loops << endl;
+    cout << "Total Program Running Time:  " << runtime << " seconds!" << endl;
+}
+
+/*****************************************************************************
+ *************** helper function to determine when convergent ****************
+ *****************************************************************************/
+
+/**
+ * function: convergenceCondition
+ ********************************
+ * compute difference between max and min dsv value in @Boxes
+ *
+ * returns: true/false value based on convergence condition
+ */
+int convergenceCondition() {
+    float max = Boxes[0].dsv;
+    float min = Boxes[0].dsv;
+    for (int i = 1; i < numBoxes; i++) {
+        if ( Boxes[i].dsv > max ) { max = Boxes[i].dsv; }
+        if ( Boxes[i].dsv < min ) { min = Boxes[i].dsv; }
+    }
+    int con = ((max - min) <= (CONVERGENCE_CONST * max)) ? 1 : 0;
+    if (con) cout << "Final Max: " << max << "; Final Min: " << min << endl;
+    return con;
+}
+
+/*****************************************************************************
+ *********************** helper function to free memory **********************
+ *****************************************************************************/
+
+/**
+ * function: freeBoxes
+ *********************
+ * free all malloc'd memory
+ */
+void freeBoxes() {
+    for (int i = 0; i < numBoxes; i++) {
+        free(Boxes[i].topNeighbor);
+        free(Boxes[i].bottomNeighbor);
+        free(Boxes[i].leftNeighbor);
+        free(Boxes[i].rightNeighbor);
+    }
+}
