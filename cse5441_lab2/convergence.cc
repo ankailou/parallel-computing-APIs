e2c5555a83f51655cb9fe1b8a1d86952706a3428
Ankai Lou <ankailou@gmail.com>
2015-11-07 21:44:10 -0500

    add line breaks to prints

--- cse5441_lab2/convergence.cc
+++ cse5441_lab2/convergence.cc
@@ -23,7 +23,7 @@ void convergenceLoop(int numThreads) {
     #pragma omp for
     for (int i = 0; i < numBoxes; i++)
         computeNewDSV(&Boxes[i]);
-        printf("Actual number of threads: %d", omp_get_num_threads());
+        printf("Actual number of threads: %d\n", omp_get_num_threads());
     }
     for (int j = 0; j < numBoxes; j++)
         Boxes[j].dsv = Boxes[j].dsvNew;
