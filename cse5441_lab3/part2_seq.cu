febe27576d75090b4127355172d05972ea5aef17
Ankai Lou <ankailou@gmail.com>
2015-11-25 15:44:14 -0500

    lab3 part2 1.0.0

---
+++ cse5441_lab3/part2_seq.cu
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define dim 4096
+
+void kernel(double **A, double **C) {
+    for (int i = 0; i < dim; i++)
+        for (int j = 0; j < dim; j++)
+            for (int k = 0; k < dim; k++)
+               C[i][j] += A[k][i] * A[k][j];
+}
+
+int main() {
+    double **A, **C;
+    A = new double*[dim];
+    C = new double*[dim];
+    // generate random array
+    for (int i = 0; i < dim; i++) {
+        A[i] = new double[dim];
+        C[i] = new double[dim];
+        for (int j = 0; j < dim; j++) {
+            A[i][j] = 1.0 + ((double)rand() / (double)RAND_MAX);
+        }
+    }
+    // call kernel
+    kernel(A,C);
+}
+
