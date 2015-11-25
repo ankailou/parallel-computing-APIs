6a44a963c3b07c60e07719ece2f50150a787da37
Ankai Lou <ankailou@gmail.com>
2015-11-25 17:36:01 -0500

    lab3 part1 1.0.5

--- cse5441_lab3/part1_par.cu
+++ cse5441_lab3/part1_par.cu
@@ -4,9 +4,13 @@
 #define dim 4097
 
 __global__ void kernel(double **F) {
-    int i = blockIdx.x + 1;
-    int j = blockIdx.y;
-    F[i][j] = F[i-1][j+1] + F[i][j+1];
+    int idx = blockIdx.x * 4 + 1;
+    int jdx = threadIdx.x * 4;
+    for (int k = 0; k < 100; k++) {
+        for (int i = idx; i < idx + 4; i++ )
+            for (int j = jdx; j < jdx + 4; j++)
+                F[i][j] = F[i-1][j+1] + F[i][j+1];
+    }
 }
 
 int main() {
@@ -14,8 +18,8 @@ int main() {
     double **d_a;  // device pointer
 
     // thread hierarchy
-    int nblocks = 4096;
-    int tpb = 100;
+    int nblocks = 1024;
+    int tpb = 1024;
 
     // allocate memory
     size_t memSize;
@@ -34,7 +38,7 @@ int main() {
 
     // launch kernel
     dim3 dimGrid(tpb);
-    dim3 dimBlock(nblocks,nblocks);
+    dim3 dimBlock(nblocks);
     kernel<<<dimGrid,dimBlock>>>(d_a);
 }
 
