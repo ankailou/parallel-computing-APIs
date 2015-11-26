741b46c6c36863bf71e455ff1b0695715a910e1c
Ankai Lou <ankailou@gmail.com>
2015-11-25 19:16:43 -0500

    lab3 part1 1.0.7

--- cse5441_lab3/part1_par.cu
+++ cse5441_lab3/part1_par.cu
@@ -3,19 +3,19 @@
 #include <math.h>
 #define dim 4097
 
-__global__ void kernel(double **F) {
+__global__ void kernel(double *F) {
     int idx = blockIdx.x * 4 + 1;
     int jdx = threadIdx.x * 4;
     for (int k = 0; k < 100; k++) {
         for (int i = idx; i < idx + 4; i++ )
             for (int j = jdx; j < jdx + 4; j++)
-                F[i][j] = F[i-1][j+1] + F[i][j+1];
+                F[(i*1024) + j] = F[1024*(i-1) + j+1] + F[(1024*i) + j+1];
     }
 }
 
 int main() {
-    double **F;    // host pointer
-    double **d_a;  // device pointer
+    double *F;    // host pointer
+    double *d_a;  // device pointer
 
     // thread hierarchy
     int nblocks = 1024;
@@ -23,16 +23,14 @@ int main() {
 
     // allocate memory
     size_t memSize;
-    F = new double*[dim];
+    F = new double[dim * dim];
     memSize = dim * dim * sizeof(double);
-    cudaMalloc((void**)&d_a, dim*sizeof(*d_a));
+    cudaMalloc((void**)&d_a, memSize);
 
     // generate random array & copy
     for (int i = 0; i < dim; i++) {
-        F[i] = new double[dim];
-        cudaMalloc((void**)&d_a[i],dim*sizeof(*d_a[i]))
         for (int j = 0; j < dim; j++) {
-            F[i][j] = 1.0 + ((double)rand() / (double)RAND_MAX);
+            F[(i*1024) + j] = 1.0 + ((double)rand() / (double)RAND_MAX);
         }
     }
     cudaMemcpy(d_a,F,memSize,cudaMemcpyHostToDevice);
